e133b8078ed7b0bab9652ef88f51e3a1f6600df2
mahmoud <mahmoud.mohamed30120@gmail.com>
2023-02-24 02:30:34 +0200

    feat(viteConfig): add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import Components from 'unplugin-vue-components/vite'
 import { configCompressPlugin } from './config/compress'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -51,6 +53,35 @@ export default defineConfig({
     Icons({
       autoInstall: true,
     }),
+    // https://github.com/antfu/vite-plugin-pwa
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
+      manifest: {
+        name: 'Modern Vue',
+        short_name: 'modern-vue',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+
     configCompressPlugin('brotli'),
   ],
 })
